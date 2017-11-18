9b76453de4ff9b3128ef8a9c05af75b1c92aa277
Alex <alex.guoxin@gmail.com>
2017-11-18 14:20:53 -0800

    Edit cookiecutter.sh

--- cookiecutter.sh
+++ cookiecutter.sh
@@ -1,3 +1,6 @@
+# this script generates a project file and directory structure for a data analysis project
+# argument: project_name (default name: "project")
+
 mkdir ${1:-project}
 
 cd ./${1:-project}
@@ -5,6 +8,8 @@ cd ./${1:-project}
 echo "# ${1:-project}" > README.md
 
 touch CITATION.md
+touch CONTRIBUTING.md
+touch CONDUCT.md
 touch LICENSE.md
 
 mkdir data
@@ -12,3 +17,4 @@ mkdir doc
 mkdir results
 mkdir src
 mkdir from_joe
+mkdir bin
